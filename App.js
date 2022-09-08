94fa7a327d3d6b688ad3fddb0b9360a8c3a44dbc
StevenJBurns <stevenjburns@msn.com>
2022-09-07 20:12:33 -0600

    feat: install boilerplate Expo foundation for multi-platform app

--- App.js
+++ App.js
@@ -4,17 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#33f',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    color: 'gold',
+  }
 });
