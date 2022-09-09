32db48d74e08c89fa7c737d42aaf022eaa618270
StevenJBurns <stevenjburns@msn.com>
2022-09-09 11:08:46 -0600

    feat: implement temp screens in App.js; no real navigation yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home, About, Athletes, References } from './components/screens';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={Home}></Stack.Screen>
+          <Stack.Screen name='About' component={About}></Stack.Screen>
+          <Stack.Screen name='Athletes' component={Athletes}></Stack.Screen>
+          <Stack.Screen name='References' component={References}></Stack.Screen>
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 };
 
