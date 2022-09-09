888f8dbc5b5ec9f69ea27ec4d5938e9e7da6093b
StevenJBurns <stevenjburns@msn.com>
2022-09-09 11:07:41 -0600

    chore: create index for screen exports

---
+++ src/components/screens/index.ts
@@ -0,0 +1,5 @@
+export { About } from './About';
+export { Athletes } from './Athletes';
+export { Home } from './Home';
+export { References } from './References';
+export { Screen } from './Screen';
