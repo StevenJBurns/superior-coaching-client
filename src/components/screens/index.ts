685c59cf5dbfdb963e26ac3af98918323ca50a63
StevenJBurns <stevenjburns@msn.com>
2022-09-13 00:39:36 -0600

    chore: remove commented import from previous fix

--- src/components/screens/index.ts
+++ src/components/screens/index.ts
@@ -2,4 +2,3 @@ export { Home } from './Home/Home';
 export { AthleteList } from './athletes/AthleteList/AthleteList';
 export { Planning } from './Planning';
 export { References } from './References';
-// export { Screen } from './Screen';
