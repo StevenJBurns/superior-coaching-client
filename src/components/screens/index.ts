b715ab2608c130fe1a8e3ac73ceb0ce03eda1b51
StevenJBurns <stevenjburns@msn.com>
2022-09-14 08:00:58 -0600

    chore: add export for CreateAthleteScreen

--- src/components/screens/index.ts
+++ src/components/screens/index.ts
@@ -1,4 +1,5 @@
 export { Home } from './Home/Home';
 export { AthleteList } from './athletes/AthleteList/AthleteList';
+export { AthleteCreateScreen } from './athletes/AthleteCreateScreen/AthleteCreateScreen';
 export { Planning } from './Planning';
 export { References } from './References';
