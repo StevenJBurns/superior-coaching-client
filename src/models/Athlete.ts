e32e928bf8b7adcc67e55dc0796ff1cf6ab28341
StevenJBurns <stevenjburns@msn.com>
2022-09-14 07:59:37 -0600

    feat: create Athlete class

--- src/models/Athlete.ts
+++ src/models/Athlete.ts
@@ -1,3 +1,26 @@
-export class Athlete {
+type NewAthleteArgs = {
+  id: string,
+  lastName: string,
+  firstName: string,
+};
 
+export class Athlete {
+  // private id: string;
+  // private lastName: string;
+  // private firstName: string;
+
+  constructor(private args: NewAthleteArgs) {
+  }
+
+  get id() {
+    return this.args.id;
+  };
+
+  get lastName() {
+    return this.args.lastName;
+  };
+  
+  get firstName() {
+    return this.args.firstName;
+  };
 };
