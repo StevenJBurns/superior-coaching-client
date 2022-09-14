366936d3f286947ab64e2b4177b4431a9f5c309d
StevenJBurns <stevenjburns@msn.com>
2022-09-14 08:02:50 -0600

    feat: implement CreateAthleteButton to AthleteList

--- src/components/screens/athletes/AthleteList/AthleteList.tsx
+++ src/components/screens/athletes/AthleteList/AthleteList.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FlatList, Pressable, Text, StyleSheet, View } from 'react-native';
+import { FlatList, Text, StyleSheet } from 'react-native';
 import { Screen } from '../../Screen';
-import { AthleteListCard } from './AthleteListCard';
+import { AthleteCreateButton } from './AthleteCreateButton';
+import { AthleteListCard } from '../AthleteListCard/AthleteListCard';
 import mockAthleteData from '../../../../data/athletes.json';
 
 type ListProps = {
@@ -9,38 +10,14 @@ type ListProps = {
 };
 
 const styles = StyleSheet.create({
-  createCardContainer: {
-    marginVertical: 12,
-    padding: 8,
-    backgroundColor: 'green',
-  },
-  createCardButton: {
-    marginVertical: 12,
-    padding: 8,
-    backgroundColor: 'lightGreen',
-  },
   text: {
     fontSize: 20,
     fontWeight: 'bold',
   },
   flatList: {
-    width: '90%',
-    maxWidth: 320,
+    width: '100%',
+    maxWidth: 400,
   },
-  itemCard: {
-    margin: 4,
-    padding: 8,
-    height: 64,
-    backgroundColor: 'silver',
-  },
-  itemLine1 : {
-    marginVertical: 2,
-    fontSize: 18
-  },
-  itemLine2: {
-    marginVertical: 2,
-    fontSize: 10
-  }
 });
 
 export const AthleteList = (props: ListProps) => {
@@ -50,11 +27,7 @@ export const AthleteList = (props: ListProps) => {
   return (
     <Screen navigation={props.navigation}>
       <Text style={styles.text}>ATHLETE CONTENT</Text>
-      <View style={styles.createCardContainer}>
-        <Pressable style={styles.createCardButton}>
-          <Text>Create New Athlete Card</Text>
-        </Pressable>
-      </View>
+      <AthleteCreateButton />
       <FlatList
         data={mockAthleteData}
         renderItem={renderItem}
