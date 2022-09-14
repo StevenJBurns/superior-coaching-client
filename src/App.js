0dea89cdafe676625d1acf023df15860cbd2d289
StevenJBurns <stevenjburns@msn.com>
2022-09-14 08:01:48 -0600

    feat: add CreateAthleteScreen to root StackNavigator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home, AthleteList, Planning, References } from './components/screens';
+import { Home, AthleteList, AthleteCreateScreen, Planning, References } from './components/screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,10 +9,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Home' component={Home}></Stack.Screen>
-        <Stack.Screen name='Athletes' component={AthleteList}></Stack.Screen>
-        <Stack.Screen name='Planning' component={Planning}></Stack.Screen>
-        <Stack.Screen name='References' component={References}></Stack.Screen>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Athletes' component={AthleteList} />
+        <Stack.Screen name='AthleteCreateScreen' component={AthleteCreateScreen} />
+        <Stack.Screen name='Planning' component={Planning} />
+        <Stack.Screen name='References' component={References} />
       </Stack.Navigator>
       <StatusBar style='auto' />
     </NavigationContainer>
