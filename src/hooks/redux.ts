4f5714ca779e799bca4416832b99da88585a5ff1
StevenJBurns <stevenjburns@msn.com>
2022-09-29 13:32:13 -0600

    chore: rename Redux dispatch and selector hooks

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -2,5 +2,5 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from '../state/redux/store/store';
 
 // Use throughout the app instead of `useDispatch` and `useSelector`
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useReduxSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useReduxDispatch: () => AppDispatch = useDispatch;
