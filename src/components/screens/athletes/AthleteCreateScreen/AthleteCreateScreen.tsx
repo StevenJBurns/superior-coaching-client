e76cbbb61fdb17035bc879aaa85a2c50883cbd29
StevenJBurns <stevenjburns@msn.com>
2022-09-29 13:48:07 -0600

    refactor: move AthleteCreateScreen styles to new file

--- src/components/screens/athletes/AthleteCreateScreen/AthleteCreateScreen.tsx
+++ src/components/screens/athletes/AthleteCreateScreen/AthleteCreateScreen.tsx
@@ -1,39 +1,11 @@
 import React from 'react';
 import { useReduxDispatch } from '../../../../hooks/redux';
 import { useNavigation, CommonActions } from '@react-navigation/native';
-import { Pressable, Text, TextInput, StyleSheet, View } from 'react-native';
+import { Pressable, Text, TextInput, View } from 'react-native';
 import { create } from '../../../../state/redux/slices/athleteSlice';
 import { Athlete } from '../../../../models/Athlete';
 import { Screen } from '../../Screen';
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  screenActionsView : {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  button: {
-    margin: 4,
-    padding: 8,
-    width: 96,
-    borderRadius: 4,
-    backgroundColor: 'lightblue',
-    textAlign: 'center',
-    color: 'white',
-  },
-  input: {
-    width: 160,
-    margin: 2,
-    padding: 8,
-    borderColor: "gray",
-    borderRadius: 4,
-    borderWidth: 1,
-  }
-});
+import styles from './AthleteCreateScreen.styles';
 
 enum athleteAction {
   setLastName,
