915576f7aaf6cc96c1e1c2dbaafeb84d4ab2faad
StevenJBurns <stevenjburns@msn.com>
2022-10-06 20:51:09 -0600

    chore: install and implement React Native Paper

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Provider as ReduxProvider } from 'react-redux'
 import { store } from './state/redux/store/store';
 import { NativeBaseProvider } from "native-base";
+import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home, AthleteListScreen, AthleteCreateScreen, Planning, References, AthleteDetailsScreen } from './components/screens';
@@ -11,19 +12,21 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <ReduxProvider store={store}>
-      <NativeBaseProvider>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name='Home' component={Home} />
-            <Stack.Screen name='Athletes' component={AthleteListScreen} />
-            <Stack.Screen name='AthleteCreate' component={AthleteCreateScreen} />
-            <Stack.Screen name='AthleteDetails' component={AthleteDetailsScreen} />
-            <Stack.Screen name='Planning' component={Planning} />
-            <Stack.Screen name='References' component={References} />
-          </Stack.Navigator>
-          <StatusBar style='auto' />
-        </NavigationContainer>
-    </NativeBaseProvider>
+      <PaperProvider>
+        <NativeBaseProvider>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name='Home' component={Home} />
+              <Stack.Screen name='Athletes' component={AthleteListScreen} />
+              <Stack.Screen name='AthleteCreate' component={AthleteCreateScreen} />
+              <Stack.Screen name='AthleteDetails' component={AthleteDetailsScreen} />
+              <Stack.Screen name='Planning' component={Planning} />
+              <Stack.Screen name='References' component={References} />
+            </Stack.Navigator>
+            <StatusBar style='auto' />
+          </NavigationContainer>
+      </NativeBaseProvider>
+    </PaperProvider>
   </ReduxProvider>
   );
 };
